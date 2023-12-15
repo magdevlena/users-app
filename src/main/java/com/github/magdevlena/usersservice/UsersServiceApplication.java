30b6106de6a257a3d5adec67142aaed863da5709
magdevlana <latkowska.magdalena@gmail.com>
2023-12-15 18:24:21 +0100

    Initial commit

---
+++ src/main/java/com/github/magdevlena/usersservice/UsersServiceApplication.java
@@ -0,0 +1,18 @@
+package com.github.magdevlena.usersservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@Slf4j
+@SpringBootApplication
+public class UsersServiceApplication {
+
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext context = SpringApplication.run(UsersServiceApplication.class, args);
+
+        log.info("Application started on port " + context.getEnvironment().getProperty("server.port"));
+    }
+
+}
