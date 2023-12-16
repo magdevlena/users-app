fd3c879a68c17ad294165fabf6262e66917a5fa2
magdevlana <latkowska.magdalena@gmail.com>
2023-12-16 22:08:34 +0100

    Fix bug regarding token header and use conditional beans for RestTemplate

--- src/main/java/com/github/magdevlena/usersservice/configuration/RestTemplateConfiguration.java
+++ src/main/java/com/github/magdevlena/usersservice/configuration/RestTemplateConfiguration.java
@@ -2,13 +2,13 @@ package com.github.magdevlena.usersservice.configuration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Optional;
 
 @Slf4j
 @Configuration
@@ -17,8 +17,8 @@ public class RestTemplateConfiguration {
     private static final String GITHUB_TOKEN_PROVIDED_LOG =
             "GitHub token provided, application will be using authenticated requests.";
     private static final String MISSING_GITHUB_TOKEN_LOG =
-            "GitHub token not provided, application will be using unauthenticated requests" +
-                    " - rate limit might be easily exceeded.";
+            "GitHub token not provided, application will be using unauthenticated requests"
+                    + " - rate limit might be easily exceeded.";
     private final String githubToken;
 
     public RestTemplateConfiguration(@Value("${github-token:#{null}}") String token) {
@@ -26,20 +26,19 @@ public class RestTemplateConfiguration {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        final var restTemplateBuilder = new RestTemplateBuilder();
-        addAuthorizationHeaderIfTokenProvided(restTemplateBuilder);
-        return restTemplateBuilder.build();
+    @ConditionalOnProperty(name = "github-token")
+    public RestTemplate restTemplateWithTokenBearer() {
+        log.info(GITHUB_TOKEN_PROVIDED_LOG);
+        return new RestTemplateBuilder()
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + githubToken)
+                .build();
     }
 
-    private void addAuthorizationHeaderIfTokenProvided(RestTemplateBuilder builder) {
-        Optional.ofNullable(githubToken)
-                .ifPresentOrElse(token -> {
-                            log.info(GITHUB_TOKEN_PROVIDED_LOG);
-                            builder.defaultHeader(HttpHeaders.AUTHORIZATION, token);
-                        },
-                        () -> log.warn(MISSING_GITHUB_TOKEN_LOG)
-                );
+    @Bean
+    @ConditionalOnMissingBean(RestTemplate.class)
+    public RestTemplate restTemplateWithoutAuthorization() {
+        log.warn(MISSING_GITHUB_TOKEN_LOG);
+        return new RestTemplate();
     }
-
 }
+
